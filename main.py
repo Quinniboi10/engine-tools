8c35fd21ddf23f2460f7fe9aa06fae21a6fe18d3
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-06-01 13:43:29 -0500

    feat(cli): simple argparse CLI + test integration

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from pathlib import Path
 from uci import UCIHandler
-from tests import *
+from tests import debug_perft, debug_perft_suite
 
 import logging
+import argparse
+
+version_string = f"Engine Tools 0.0.1"
+
+parser = argparse.ArgumentParser(
+                    prog=version_string,
+                    description="Various Python tools to make engine debugging easier")
+parser.add_argument("-e", "--engine", help="Primary engine to run tests on", required=True)
+parser.add_argument("-r", "--reference-engine", help="Reference engine to use as a baseline", required=True)
+
+parser.add_argument("--perft-pos", help="Debug an individual perft position, formatted <fen>:<depth>")
+parser.add_argument("--perft-suite", help="Path to an standard .epd file")
+
+args = parser.parse_args()
 
 def main():
     logging.basicConfig(filename="engine-bench.log", level=logging.DEBUG)
 
-    engine = UCIHandler(Path("../Chess/Lazarus/Lazarus.exe"))
+    engine = UCIHandler(Path(args.engine))
 
-    working_eng = UCIHandler(Path("../Chess/Chaos/Chaos.exe"))
+    working_eng = UCIHandler(Path(args.reference_engine))
 
-    debug_perft_suite(Path("../../Downloads/standard.epd"), working_eng, engine)
+    if args.perft_pos is not None:
+        fen, depth = args.perft_pos.strip().split(":")
+        debug_perft(fen, int(depth), working_eng, engine)
+    if args.perft_suite is not None:
+        debug_perft_suite(Path(args.perft_suite), working_eng, engine)
 
 if __name__ == "__main__":
     main()
